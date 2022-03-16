d3a8b9da4e8a9671f653d0e230e07148b5a5c662
AzureAD\MikhailTushev <mikhail.tushev@itl.team>
2022-03-16 23:47:15 +0300

    Added my example of solving the problem of generating a mask for the selected keywords second path

---
+++ Portfolio.Web/Controllers/MaskController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.ApplicationServices.Example;
+using Portfolio.Common.Consts;
+
+namespace Portfolio.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MaskController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("/mask_conditions")]
+        public Dictionary<string, string> GetMaskConditions()
+        {
+            return MaskConst.Values;
+        }
+
+        [HttpPost]
+        public Task<string> CreateNotification([FromQuery] GenerateMaskCommand request)
+        {
+            return _mediator.Send(request);
+        }
+    }
+}
